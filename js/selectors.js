3477448437e9d7f746cae3c7cc25ff8381eaa511
Phan Thanh Hoàng <5751023016@st.utc2.edu.vn>
2022-10-10 23:10:32 +0700

    event delegation

--- js/selectors.js
+++ js/selectors.js
@@ -4,11 +4,11 @@
 // 3. Replay Game
 // 4. Game status
 export function getCellElementList() {
-  return document.querySelectorAll('ul#cellList > li');
+  return document.querySelectorAll("ul#cellList > li");
 }
 
 export function getCurrentTurnElement() {
-  return document.getElementById('currentTurn');
+  return document.getElementById("currentTurn");
 }
 
 export function getCellElementAtIdx(index) {
@@ -16,9 +16,13 @@ export function getCellElementAtIdx(index) {
 }
 
 export function getGameStatusElement() {
-  return document.getElementById('gameStatus');
+  return document.getElementById("gameStatus");
 }
 
 export function getReplayButtonElement() {
-  return document.getElementById('replayGame');
+  return document.getElementById("replayGame");
+}
+
+export function getCellListElement() {
+  return document.getElementById("cellList");
 }
