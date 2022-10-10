1045a6d0e70513fe24f1f918540e92407aec1363
Ong Hoang Di Code <onghoangdicode@gmail.com>
2022-10-10 11:00:01 +0700

    add selectors

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+import {
+  getCellElementAtIdx,
+  getCellElementList,
+  getCurrentTurnElement,
+  getGameStatusElement,
+} from "./selectors.js";
+
+console.log(getCellElementList());
+console.log(getCurrentTurnElement());
+console.log(getCellElementAtIdx(3));
+console.log(getGameStatusElement());
+
 /**
  * Global variables
  */
